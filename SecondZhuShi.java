b50f209960e39254d62eba641fc446fbb506d9f2
daxialishiliang <daxialishiliang@163.com>
2021-01-26 15:05:39 +0800

    '注释2'

---
+++ SecondZhuShi.java
@@ -0,0 +1,15 @@
+/*
+		注释的作用：
+				A：解释说明程序，提高程序的阅读性
+				B:可以帮助我们调试程序。
+						
+*/
+class SecondZhuShi{
+		public static void main (String[]args){
+				System.out.println("李小龙");
+				System.out.println("刘德华");
+				System.out.println("林青霞");
+				System.out.println("anglebaby");
+		}
+
+}	
