1cce18a04e86354e4da47e89ce6018d3c219279a
daxialishiliang <daxialishiliang@163.com>
2021-01-27 15:43:06 +0800

    'boolean'

--- OperatorDemo.java
+++ OperatorDemo.java
@@ -1,30 +1,39 @@
 /*
-	赋值运算符：
-	
-			基本的赋值运算符：=
-						把“=右边的数据赋值给左边。
-			
-			拓展的赋值运算符：+=，-=，*=，/=，%=
-						+=把左边和右边做加法，然后赋值给左边。
+		比较运算符：
+					==，！=，>,>=,<,<=
+		
+		特点：
+					无论你的操作是简单还是复杂，结果是boolean类型。
+		
+		注意事项：
+					“==”不能写成“=”.
 */
 class OperatorDemo{
-		public static void main(String[] args){
-				//定义一个变量
-				int x=10;
+	  public static void main(String[]args){
+				int x=3;
+				int y=4;
+				int z=3;
 				
-				//其他用法
-				int a,b;
-				a=b=10;
-				System.out.println(a);
-				System.out.println(b);
-				System.out.println("-----------");
+				System.out.println(x==y);
+				System.out.println(x==z);
+				System.out.println((x+y)==(x+z));
+				System.out.println("----------------");
 				
-				//定义一个变量
-                 int y=10;
-				 y +=20;
-				 System.out.println(y);
-
+				System.out.println(x!=y);
+				System.out.println(x>y);
+				System.out.println(x>=y);
+				System.out.println(x<y);
+				System.out.println(x<=y);
+				System.out.println("----------------");
+				
+				int a=10;
+				int b=20;
+				
+				//boolean flag=(a==b);
+				//boolean flag=(a=b);//这个是有问题的，不兼容的类型
+				//System.out.println(flag);
+				
+				int c=(a=b);//把b赋值给a，然后把a留下来
+				System.out.println(c);
 				}
-
-}						
-			
+}					
