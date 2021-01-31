ff27d58f4a8d61738fdc07c9872adad155e93bf3
daxialishiliang <daxialishiliang@163.com>
2021-01-31 11:42:02 +0800

    'if'

---
+++ if.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>网页标题</title>
+<meta name="keywords"content="关键字列表"/>
+<meta name="description"content="网页描述"/>
+<link rel="stylesheet"type="text/css"href=""/>
+<style type="text/css"></style>
+<script type="text/javascript"></script>
+</head>
+
+<body>
+//*
+echo "<br />代码1";
+echo"<br />代码2"；
+echo"<br/>代码3，代表多行需要注释的 diamante"
+//*/
+if(1==1){
+	echo"<br />代码1";
+    echo"<br />代码2"；
+    echo"<br />代码3，代码多行需要注释的代码”}
+
+
+
+
+</body>
+</html>
