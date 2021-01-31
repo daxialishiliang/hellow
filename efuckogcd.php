114825e787740f1e9283e31410109d36769382af
daxialishiliang <daxialishiliang@163.com>
2021-01-31 10:57:44 +0800

    'fgcd'

---
+++ efuckogcd.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+<meta name="keyword" content="关键字列表"/>
+<meta name="description" content="网页描述"/>
+<link rel="stylesheet"type="text/css"href=""/>
+<style type="text/css"></style>
+<script type="text/javascript"></script>
+</head>
+<body>
+<?php
+echo"<br />abc11";
+echo"<br/>abc12";
+echo"<br />abc13"
+?>
+<hr />
+<script language="php">
+echo"<br />abc2";
+</script>
+<hr />
+<?
+echo "<br />abc3";
+?>
+</body>
+</html>
+echo"<br/>这是最后一行的PHP代码，气候没有其他代码了"；
+echo"<br/>这是最后的phpdaima ,qihou meiyo uqita daima le ";
+echo"<br />这是最后的php代码，气候没有其他代码了" 
