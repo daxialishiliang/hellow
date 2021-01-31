7b37481066cc7c6611dda5156359ea8cb22fe471
daxialishiliang <daxialishiliang@163.com>
2021-01-31 12:34:31 +0800

    'd'

---
+++ fgcd.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>网页标题</title>
+<meta name="keywords" content="光阿金自列表"/>
+<meta name="description"content="网页描述"/>
+<link rel="stylesheet"type="text/css"href=""/>
+<style type="text/css"></style>
+<script type="text/javascript"></script>
+</head>
+
+<body>
+<?php
+$v1=isset($s1);
+$s2=2;
+$v2=isset($s2);//true
+
+$s3=false;
+$v3=isset($s3);
+$s4="";
+$v4=isset($s4);
+$s5=null;
+$v5=isset($s5);
+?>
+</body>
+</html>
