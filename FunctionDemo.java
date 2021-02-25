634a912c090c91bf20ac5781adcee54bd43dac64
daxialishiliang <daxialishiliang@163.com>
2021-02-25 15:52:13 +0800

    ''

---
+++ FunctionDemo.java
@@ -0,0 +1,14 @@
+class FunctionDemo{
+	public static void main(String[] args){
+			int x=10;
+			int y=20;
+			int result =sum(x,y);
+			System.out.println(result);
+	}
+	public static int sum(int a,int b){
+				return a+b;
+	}
+
+
+
+}
