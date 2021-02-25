754ae54b9fd3d5b1a140ed81b618e0e6ebd60020
daxialishiliang <daxialishiliang@163.com>
2021-02-25 16:06:38 +0800

    ''

---
+++ FunctionDemo3.java
@@ -0,0 +1,15 @@
+class FunctionDemo3{
+		public static void main(String[] args){
+				for(int x=0;x<100;x++){
+						for(int y=0;y<55;y++){
+								System.out.print("*");
+						}
+						System.out.println();				
+				}
+				System.out.println("---------------");
+		
+		
+		}
+
+
+}
